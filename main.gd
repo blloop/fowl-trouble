0680dee6f91c3d5e6567240a99f772283a4332d7
blloop <42186044+blloop@users.noreply.github.com>
2024-02-10 10:12:22 -0800

    add save and load features

--- main.gd
+++ main.gd
@@ -1,12 +1,8 @@
 extends Node2D
 
-# Called when the node enters the scene tree for the first time.
-#func _ready():
-	#pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-	#pass
+func _ready():
+	Utils.save_game()
+	Utils.load_game()
 
 func _on_quit_pressed():
 	get_tree().quit()
--- player/player.gd
+++ player/player.gd
@@ -1,6 +1,5 @@
 extends CharacterBody2D
 
-var health = 10
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
@@ -35,7 +34,7 @@ func _physics_process(delta):
 	if velocity.y > 0:
 		anim.play("Fall")
 		
-	if health <= 0:
+	if Game.player_hp <= 0:
 		queue_free()
 		get_tree().change_scene_to_file("res://main.tscn")
 
