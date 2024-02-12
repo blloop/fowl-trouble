17cafeefc449657bd3776ca815baf91213080f94
blloop <42186044+blloop@users.noreply.github.com>
2024-02-11 17:35:04 -0800

    add start menu options for more levels

--- main.gd
+++ main.gd
@@ -8,4 +8,10 @@ func _on_quit_pressed():
 	get_tree().quit()
 
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://world.tscn")
+	#get_tree().change_scene_to_file("res://world.tscn")
+	get_node("Start").visible = false
+	get_node("Select").visible = true
+
+func _on_return_pressed():
+	get_node("Start").visible = true
+	get_node("Select").visible = false
