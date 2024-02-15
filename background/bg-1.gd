77ab915445c993cdc9e2c6a7885a1fc47fc7ac0e
blloop <42186044+blloop@users.noreply.github.com>
2024-02-15 15:25:38 -0800

    add font, improve pause screen design

---
+++ background/bg-1.gd
@@ -0,0 +1,9 @@
+extends ParallaxBackground
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	get_node("ParallaxLayer/Water1").play("Idle")
+	get_node("ParallaxLayer/Water2").play("Idle")
+	get_node("ParallaxLayer/Water3").play("Idle")
+	get_node("ParallaxLayer/Water4").play("Idle")
+	get_node("ParallaxLayer/Water5").play("Idle")
