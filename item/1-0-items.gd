f0f5781bf5705605e8faa38e6504535db17a3ed3
blloop <42186044+blloop@users.noreply.github.com>
2024-02-15 00:15:26 -0800

    remove unused features

--- item/1-0-items.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-var cherry = preload("res://item/cherry.tscn")
-
-func _on_timer_timeout():
-	var cherry_temp = cherry.instantiate()
-	var rng = RandomNumberGenerator.new()
-	cherry_temp.position.x += rng.randi_range(-80, 80)
-	add_child(cherry_temp)
