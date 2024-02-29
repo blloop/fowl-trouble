19edc5fdbe323999905e764602676adb4ea457ea
blloop <42186044+blloop@users.noreply.github.com>
2024-02-28 17:08:51 -0800

    add level 1-1 features

--- ui/sign-layer.gd
+++ ui/sign-layer.gd
@@ -2,17 +2,10 @@ extends Node2D
 
 var tween : Tween
 
-func _ready():
-	tween = create_tween()
-
-func _process(_delta):
-	#if tween_a.
-	pass
-
 func open_sign():
+	tween = get_tree().create_tween()
 	tween.tween_property(self, "scale", Vector2(1, 1), 0.5)
-	tween.play()
 
 func close_sign():
+	tween = get_tree().create_tween()
 	tween.tween_property(self, "scale", Vector2(0, 0), 0.5)
-	tween.play()
