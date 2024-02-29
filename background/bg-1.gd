7858611aed229fde0134aff5434a892c72e5a7b6
blloop <42186044+blloop@users.noreply.github.com>
2024-02-28 16:40:15 -0800

    add sun bg and parallax

--- background/bg-1.gd
+++ background/bg-1.gd
@@ -2,7 +2,7 @@ extends ParallaxBackground
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	get_node("ParallaxLayer/Water1").play("Idle")
-	get_node("ParallaxLayer/Water2").play("Idle")
-	get_node("ParallaxLayer/Water3").play("Idle")
-	get_node("ParallaxLayer/Water4").play("Idle")
+	get_node("ParallaxLayer3/Water1").play("Idle")
+	get_node("ParallaxLayer3/Water2").play("Idle")
+	get_node("ParallaxLayer3/Water3").play("Idle")
+	get_node("ParallaxLayer3/Water4").play("Idle")
--- ui/sign-layer.gd
+++ ui/sign-layer.gd
@@ -1,29 +1,18 @@
 extends Node2D
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+var tween : Tween
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _ready():
+	tween = create_tween()
+
+func _process(_delta):
+	#if tween_a.
 	pass
 
-func open_scale():
-	self.scale.x = 1
-	self.scale.y = 1
-
-func close_scale():
-	self.scale.x = 0
-	self.scale.y = 0
-
 func open_sign():
-	var tween_a = get_tree().create_tween()
-	tween_a.tween_property(self, "scale", Vector2(1, 1), 0.5)
-	#tween_a.tween_callback(open_scale)
-	tween_a.play()
+	tween.tween_property(self, "scale", Vector2(1, 1), 0.5)
+	tween.play()
 
 func close_sign():
-	var tween_a = get_tree().create_tween()
-	tween_a.tween_property(self, "scale", Vector2(0, 0), 0.5)
-	tween_a.play()
-	#close_scale()
+	tween.tween_property(self, "scale", Vector2(0, 0), 0.5)
+	tween.play()
