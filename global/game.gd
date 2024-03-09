41904fd12f555b86bc1bcf4229cfdec7fa276dea
blloop <42186044+blloop@users.noreply.github.com>
2024-03-09 00:14:32 -0800

    add world navigation in main menu

--- global/game.gd
+++ global/game.gd
@@ -2,5 +2,6 @@ extends Node
 
 var player_hp = 10
 var gold
+var visited = false
 var paused = false
 var recap = false
--- main.gd
+++ main.gd
@@ -1,21 +1,51 @@
 extends Node2D
 
-func _ready():
-	Utils.save_game()
-	Utils.load_game()
-	get_node("Start").visible = true
-	get_node("Worlds").visible = false
+@onready var start = $Start
+@onready var worlds = $Worlds
+@onready var world_1 = get_node("World 1")
+@onready var back = $Back
 
+func _ready():
+	# Utils.save_game()
+	# Utils.load_game()
+	start.visible = !Game.visited
+	worlds.visible = Game.visited
+	world_1.visible = false
+	back.visible = false
+	
+	# Mute locked worlds
+	get_node("Worlds/Buttons/Button2").disabled = true
+	get_node("Worlds/Buttons/Button3").disabled = true
+	get_node("Worlds/Buttons/Button4").disabled = true
+	get_node("Worlds/Buttons/Button5").disabled = true
+	get_node("Worlds/Buttons/Button6").disabled = true
+	#get_node("Worlds/Buttons/Button2").set("theme_override_colors/font_color", Color(0, 0, 0, 0.5))
+	
 func _on_quit_pressed():
 	get_tree().quit()
 
 func _on_play_pressed():
-	get_node("Start").visible = false
-	get_node("Select").visible = true
+	worlds.visible = true
+	start.visible = false
+	Game.visited = true
 
 func _on_return_pressed():
-	get_node("Start").visible = true
-	get_node("Select").visible = false
+	start.visible = true
+	worlds.visible = false
+
+func _on_back_pressed():
+	worlds.visible = true
+	world_1.visible = false
+	back.visible = false
+
+# World Buttons
+
+func _on_button_1_pressed():
+	world_1.visible = true
+	worlds.visible = false
+	back.visible = true
+
+# Level Buttons
 
 func _on_level_1_pressed():
 	get_tree().change_scene_to_file("res://world-1/level-1.tscn")
