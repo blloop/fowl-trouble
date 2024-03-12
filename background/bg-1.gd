b3c6618c8d0c6cb8780ba0b30f9bf288e300c749
blloop <42186044+blloop@users.noreply.github.com>
2024-03-11 22:15:13 -0700

    add movement info graphics to sign

--- background/bg-1.gd
+++ background/bg-1.gd
@@ -2,6 +2,7 @@ extends ParallaxBackground
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	get_node("ParallaxLayer/Sun").play("Idle")
 	get_node("ParallaxLayer3/Water1").play("Idle")
 	get_node("ParallaxLayer3/Water2").play("Idle")
 	get_node("ParallaxLayer3/Water3").play("Idle")
